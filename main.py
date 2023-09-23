a19e1505af5345c3c5781983eefee77a62ced441
CrowRoth <dustinrileywork@gmail.com>
2023-09-23 15:36:41 -0600

    Got pandas to read excel dataset for vacent homes

--- main.py
+++ main.py
@@ -5,21 +5,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import pathlib as Path
 
 # (5/5 points) Initial comments with your name, class and project at the top of your .py file.
 # (5/5 points) Proper import of packages used.
 
-# get overall homeless data from dataset
-data = pd.read_excel("2007-2022-PIT-Counts-by-State.xlsx", sheet_name= "2022", index_col=0)
+#create charts folder
+# try:
+#     Path("charts").mkdir()
+# except FileExistsError:
+#     pass
 
-print(data["Overall Homeless, 2022"]["Total"])
 # (20/20 points) Using a data source of your choice, such as data from data.gov or using the Faker package, generate or retrieve some data for creating basic statistics on. This will generally come in as json data, etc.
 # Think of some question you would like to solve such as:
 # "How many homes in the US have access to 100Mbps Internet or more?"
 # "How many movies that Ridley Scott directed is on Netflix?" - https://www.kaggle.com/datasets/shivamb/netflix-shows
 # Here are some other great datasets: https://www.kaggle.com/datasets
 
+# get overall homeless data from dataset
+hlData = pd.read_excel("2007-2022-PIT-Counts-by-State.xlsx", sheet_name= "2022", index_col=0)
 
+print(hlData["Overall Homeless, 2022"]["Total"])
+
+# get vacant house data from dataset
+vhData = pd.read_excel("ann22t_11.xlsx", index_col=0)
+
+print(vhData["2022 Estimate"]["Vacant"])
 
 # (10/10 points) Store this information in Pandas dataframe. These should be 2D data as a dataframe, meaning the data is labeled tabular data.
 # (10/10 points) Using matplotlib, graph this data in a way that will visually represent the data. Really try to build some fancy charts here as it will greatly help you in future homework assignments and in the final project.
