303ee38ac46b8329cd43d4abf562ec105ab4d9fa
CrowRoth <dustinrileywork@gmail.com>
2023-09-22 22:54:55 -0600

    change of plans since api.data.gov doesnt have any actual data

--- README.md
+++ README.md
@@ -3,17 +3,14 @@ INF601 Advanced Programing with Python
 
 Dustin Riley
 ## Description
-This project will be using the packages NumPy, Matplotlib, and Pandas in order to create dataframes. This isn't intended to be full blown data science project. The goal here is to come up with some question and then see what API or datasets you can use to get the information needed to answer that question
+This project will be using the packages NumPy, Matplotlib, and Pandas in order to create dataframes. This isn't intended to be full blown data science project. The goal here is to come up with some question and then see what API or datasets you can use to get the information needed to answer that question.
+
+### Question:
+Are there more empty homes than homeless people in USA?
+
 ## Pip install instructions
 Please run the following:
 ```
 pip install -r requirements.txt
 ```
 ## Usage
-Go to https://api.data.gov/signup/ and signup for an API key.
-Then create a config.py file and add it to .gitignore.
-
-Inside config.py:
-```
-key = "Your API key here"
-```
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Dustin Riley
 # Mini Project 2
 
-import pandas
+import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import config
@@ -10,13 +10,16 @@ import config
 # (5/5 points) Initial comments with your name, class and project at the top of your .py file.
 # (5/5 points) Proper import of packages used.
 
-key = config.key
+
+
 
 # (20/20 points) Using a data source of your choice, such as data from data.gov or using the Faker package, generate or retrieve some data for creating basic statistics on. This will generally come in as json data, etc.
 # Think of some question you would like to solve such as:
 # "How many homes in the US have access to 100Mbps Internet or more?"
 # "How many movies that Ridley Scott directed is on Netflix?" - https://www.kaggle.com/datasets/shivamb/netflix-shows
 # Here are some other great datasets: https://www.kaggle.com/datasets
+
+
 
 # (10/10 points) Store this information in Pandas dataframe. These should be 2D data as a dataframe, meaning the data is labeled tabular data.
 # (10/10 points) Using matplotlib, graph this data in a way that will visually represent the data. Really try to build some fancy charts here as it will greatly help you in future homework assignments and in the final project.
